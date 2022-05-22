131a4a6ca4a3e2f98407c3206982107749d78a20
zeelmehta3107 <zeelmehta.zm31@gmail.com>
2022-05-22 17:35:00 +0530

    first commit

---
+++ ToDo/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Task(models.Model):
+    username = models.ForeignKey(User, on_delete=models.CASCADE, blank=False, null=False)
+    title = models.CharField(max_length=50)
+    work = models.CharField(max_length=250)
+    is_completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
